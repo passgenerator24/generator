a2d860fd9106b98be54d017120afda4e751b8c6a
sarahyosef <sarahyosefux@gmail.com>
2024-10-22 10:30:24 -0400

    rm old crud functions

--- crud/rest/src/main/java/com/example/rest/Repo/CredentialRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.rest.Repo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.rest.Models.Credentials;
-
-public interface CredentialRepo extends JpaRepository<Credentials, Long> {
-    
-}
